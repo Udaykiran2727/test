f86be67fab1fde52586dd0801960a775c8744f02
uday <udayasu.2004@gmail.com>
2024-10-19 10:32:15 +0530

    deleted comments

--- server.py
+++ server.py
@@ -31,22 +31,17 @@ def handle(client):
             break
 def receive():
     while True:
-        # Accept Connection
         client, address = server.accept()
         print("Connected with {}".format(str(address)))
 
-        # Request And Store Nickname
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
         nicknames.append(nickname)
         clients.append(client)
-
-        # Print And Broadcast Nickname
         print("Nickname is {}".format(nickname))
         broadcast("{} joined!".format(nickname).encode('ascii'))
         client.send('Connected to server!'.encode('ascii'))
 
-        # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 receive()
